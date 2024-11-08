43798bdca9e87aa594ebb86db79bb384ef62bb31
lmh10233 <95402181+lmh10233@users.noreply.github.com>
2024-11-08 10:26:48 +0800

    Update total_finetune.py

--- test/total_finetune.py
+++ test/total_finetune.py
@@ -17,21 +17,8 @@ import torchvision
 from warmup_scheduler import GradualWarmupScheduler
 import time
 
-
-def seed_torch(seed=3407):  # 114514, 3407
-    random.seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed) # 为了禁止hash随机化，使得实验可复现
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    # torch.cuda.manual_seed(seed)
-    # torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
-
     
-def main(train, test):
-    seed_torch()
-    
+def main(train, test):    
     fine_tune = True
     total_training_time = 0  # 初始化总训练时间
     
