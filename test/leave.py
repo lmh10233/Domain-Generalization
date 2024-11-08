5da4292cc313c7022868918c6e89a97bc9c38f59
lmh10233 <95402181+lmh10233@users.noreply.github.com>
2024-11-08 10:27:12 +0800

    Update leave.py

--- test/leave.py
+++ test/leave.py
@@ -17,21 +17,8 @@ from resnet import resnet18
 from regressor import Gaze_regressor
 from warmup_scheduler import GradualWarmupScheduler
 
-
-def seed_torch(seed=3407):  # 114514, 3407
-    random.seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed) # 为了禁止hash随机化，使得实验可复现
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
-
     
 def main(train, test):
-    
-    seed_torch()
     fine_tune = True
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
