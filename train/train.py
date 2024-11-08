b12bdbd1c4ed48a600e47a13a93194bfa98704e9
lmh10233 <95402181+lmh10233@users.noreply.github.com>
2024-11-08 10:26:19 +0800

    Update train.py

--- train/train.py
+++ train/train.py
@@ -20,17 +20,6 @@ from warmup_scheduler import GradualWarmupScheduler
 from scheduler import cosine_schedule
 from easydict import EasyDict as edict
 from torch.nn.functional import one_hot
-
-
-def seed_torch(seed=3407):  # 114514, 3407
-    random.seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed) # 为了禁止hash随机化，使得实验可复现
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
     
     
 def select_layers(layer_wise_prob):
@@ -45,7 +34,6 @@ def select_layers(layer_wise_prob):
     
 
 def main(config, model_type):
-    seed_torch()
     total_training_time = 0  # 初始化总训练时间
     dataloader = importlib.import_module("reader." + config.reader)
     
